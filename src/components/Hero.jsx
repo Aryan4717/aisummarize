e92dabe5d218802d513ee7ce8f2984443bcabab8
Aryan4717 <ak1710650@gmail.com>
2024-09-11 14:55:19 +0530

    fmcdciut commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/Aryan4717/ai_summarize", "_blank")
+            window.open("https://github.com/Aryan4717/aisummarize", "_blank")
           }
           className='black_btn'
         >
